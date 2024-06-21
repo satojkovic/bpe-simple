82fc025d52aaa0b71a8d169a773a276085dd31be
satojkovic <satojkovic@gmail.com>
2024-06-21 23:19:00 +0900

    Calc stats

--- bpe_sample.py
+++ bpe_sample.py
@@ -1,3 +1,10 @@
+def get_stats(ids):
+    counts = {}
+    for pair in zip(ids, ids[1:]):
+        counts[pair] = counts.get(pair, 0) + 1
+    return counts
+
+
 text = "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception."
 
 tokens = text.encode("utf-8")  # raw bytes
@@ -8,3 +15,6 @@ print("length:", len(text))
 print("---")
 print(tokens)
 print("length:", len(tokens))
+
+stats = get_stats(tokens)
+print(sorted(((v, k) for k, v in stats.items()), reverse=True))
