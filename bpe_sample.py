568b16c15ce8d554dbd7eaf1a99e89452b8910b5
satojkovic <satojkovic@gmail.com>
2024-06-23 18:37:22 +0900

    Merge consecutive occurrences of pairs with new token index

--- bpe_sample.py
+++ bpe_sample.py
@@ -5,6 +5,19 @@ def get_stats(ids):
     return counts
 
 
+def merge(ids, pair, idx):
+    newids = []
+    i = 0
+    while i < len(ids):
+        if i < len(ids) - 1 and ids[i] == pair[0] and ids[i + 1] == pair[1]:
+            newids.append(idx)
+            i += 2
+        else:
+            newids.append(ids[i])
+            i += 1
+    return newids
+
+
 text = "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception."
 
 tokens = text.encode("utf-8")  # raw bytes
@@ -18,3 +31,10 @@ print("length:", len(tokens))
 
 stats = get_stats(tokens)
 print(sorted(((v, k) for k, v in stats.items()), reverse=True))
+
+# Replace all consecutive occurrences of pair with the new token idx
+print(merge([5, 6, 6, 7, 9, 1], [6, 7], 99))
+top_pair = max(stats, key=stats.get)
+tokens2 = merge(tokens, top_pair, 256)
+print(tokens2)
+print("length:", len(tokens2))
